53bb5ffd358494c9d279854dedec847ea940dbed
Nero <david.zeballos@alumnos.ucn.cl>
2024-09-21 01:27:54 -0300

    Corrección controladores con ObjectId

--- SearchService/Services/MongoDBService.cs
+++ SearchService/Services/MongoDBService.cs
@@ -1,6 +1,9 @@
+using MongoDB.Bson;
 using MongoDB.Driver;
-using Microsoft.Extensions.Options;
 using SearchService.Models;
+using Microsoft.Extensions.Options;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace SearchService.Services
 {
@@ -14,20 +17,33 @@ namespace SearchService.Services
             var database = client.GetDatabase(settings.Value.DatabaseName);
             _students = database.GetCollection<Student>("students");
         }
-        
-        public async Task<List<Student>> GetStudentsAsync() =>
+
+        // Obtener todos los estudiantes
+        public async Task<List<Student>> GetAllStudentsAsync() =>
             await _students.Find(student => true).ToListAsync();
 
-        // Obtener un estudiante por su ID
-        public async Task<Student> GetStudentByIdAsync(string id) =>
-            await _students.Find(student => student.Id == id).FirstOrDefaultAsync();
-
-        // Obtener estudiantes dentro de un rango de notas
-        public async Task<List<Student>> GetStudentsByGradeRange(double min, double max) =>
-            await _students.Find(student => student.Grades.Any(g => g.GradeValue >= min && g.GradeValue <= max)).ToListAsync();
+        // Obtener un estudiante por su ObjectId
+        public async Task<Student> GetStudentByIdAsync(ObjectId id)
+        {
+            return await _students.Find(student => student.Id == id).FirstOrDefaultAsync();
+        }
 
         // Obtener estudiantes por una restricción específica
-        public async Task<List<Student>> GetStudentsByRestriction(string restrictionReason) =>
-            await _students.Find(student => student.Restrictions.Any(r => r.Reason.Contains(restrictionReason))).ToListAsync();
+        public async Task<List<Student>> GetStudentsByRestriction(string restrictionReason)
+        {
+            if (string.IsNullOrEmpty(restrictionReason))
+                return new List<Student>();
+
+            return await _students.Find(student => student.Restrictions.Any(r => r.Reason.Contains(restrictionReason))).ToListAsync();
+        }
+
+        // Obtener estudiantes dentro de un rango de notas
+        public async Task<List<Student>> GetStudentsByGradeRange(double min, double max)
+        {
+            if (min < 0 || max < 0 || min > max)
+                return new List<Student>();
+
+            return await _students.Find(student => student.Grades.Any(g => g.GradeValue >= min && g.GradeValue <= max)).ToListAsync();
+        }
     }
 }
